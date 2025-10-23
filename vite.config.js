cf77c254942f96884c711df559af6c8e284e7166
jyan0253 <jyan0253@student.monash.edu>
2025-10-24 05:37:48 +1100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+const repoBase = '/5032/'
+
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/NoMash-Library-week-4-end/' : '/',
+  base: command === 'build' ? repoBase : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
